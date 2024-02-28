78a28b078a6835d2782ff1500b12f21a1099b60d
John <john@munro.cc>
2024-02-28 16:42:27 +1300

    Update const.py

--- tests/const.py
+++ tests/const.py
@@ -1,4 +1,4 @@
-"""Constants for Bermuda BLE Trilateration tests."""
+"""Constants for Bermuda BLE Trilateration tests."""  # fmt: skip
 # AJG: We don't use these vars in our flow. TODO: Add some we _do_ use!
 # from custom_components.bermuda.const import (
 #    CONF_PASSWORD,
