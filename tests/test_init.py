0dedfa49b35d880c7b6b996e8e535c4d8612799d
John <john@munro.cc>
2024-02-28 16:43:05 +1300

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,4 +1,4 @@
-"""Test Bermuda BLE Trilateration setup process."""
+"""Test Bermuda BLE Trilateration setup process."""  # fmt: skip
 import pytest
 from custom_components.bermuda import (
     async_reload_entry,
