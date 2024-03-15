d553a5eed29f1eb5dc68afa24e3346a56ea55ba4
Ashley Gittins <ash@ajg.net.au>
2024-03-15 15:30:30 +0000

    feat: Link entities with existing devices like bthome
    - Should match on bluetooth entities.

--- custom_components/bermuda/entity.py
+++ custom_components/bermuda/entity.py
@@ -6,11 +6,12 @@ from typing import TYPE_CHECKING
 
 from homeassistant.core import callback
 from homeassistant.helpers import area_registry
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTION
+from .const import BEACON_IBEACON_DEVICE
 from .const import DOMAIN
-from .const import NAME
 
 if TYPE_CHECKING:
     from . import BermudaDataUpdateCoordinator
@@ -50,12 +51,25 @@ class BermudaEntity(CoordinatorEntity):
     @property
     def device_info(self):
         """Implementing this creates an entry in the device registry."""
+
+        # Match up our entity with any existing device entries.
+        # For scanners we use ethernet MAC, which looks like they are
+        # normally stored lowercased, otherwise we use our btmac, which
+        # seem to be stored uppercased.
+        if self._device.is_scanner:
+            connection = {(dr.CONNECTION_NETWORK_MAC, self._device.address.lower())}
+        elif self._device.beacon_type == BEACON_IBEACON_DEVICE:
+            # ibeacon doesn't (yet) actually set a connection, but
+            # this "matches" what it stores for identifier.
+            connection = {("ibeacon", self._device.address.lower())}
+        else:
+            connection = {(dr.CONNECTION_BLUETOOTH, self._device.address.upper())}
+
         return {
             "identifiers": {(DOMAIN, self._device.unique_id)},
+            "connections": connection,
             "name": self._device.prefname,
             # TODO: Could use this to indicate tracker type (IRK, iBeacon etc).
-            "model": None,
-            "manufacturer": NAME,
         }
 
     @property
