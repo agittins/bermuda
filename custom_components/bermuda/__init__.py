c29f1fe3de91bffd8560aa0b3ab4011c75bec4dd
Luke <conway220@gmail.com>
2024-11-11 11:39:49 -0500

    Add on unload

--- custom_components/bermuda/__init__.py
+++ custom_components/bermuda/__init__.py
@@ -50,6 +50,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: BermudaConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    entry.async_on_unload(coordinator.stop_purging)
+
     return True
 
 
