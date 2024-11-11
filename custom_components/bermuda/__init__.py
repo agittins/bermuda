19b54c99ecf9163c368e19c00c0be94eb762a9e2
Luke <conway220@gmail.com>
2024-11-11 11:46:00 -0500

    Move to init so it only gets run after startup

--- custom_components/bermuda/__init__.py
+++ custom_components/bermuda/__init__.py
@@ -50,7 +50,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: BermudaConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-    entry.async_on_unload(coordinator.stop_purging)
+
+    # Run it once so it will schedule itself in the future.
+    hass.loop.call_soon_threadsafe(hass.async_create_task, coordinator.purge_redactions(hass))
 
     return True
 
